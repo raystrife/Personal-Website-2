7947ae6070d3f8014b4ba818a8f9433910c38542
raystrife <rayadrian96@yahoo.com>
2022-05-01 16:23:49 -0400

    chore: revise text

--- js/index.js
+++ js/index.js
@@ -1,59 +1,67 @@
+// Methods to deal with opening and closing the nav bar
 
-// $(document).ready(function() {
-	const navToggle = document.querySelector('.nav-toggle');
-	const navLinks = document.querySelectorAll('.nav__link')
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
+function openNavBar() {
 	navToggle.addEventListener('click', () => {
 		document.body.classList.toggle('nav-open');
 	})
+}
+openNavBar();
 
+function closeNavBar() {
 	navLinks.forEach(link => {
 		link.addEventListener('click', () => {
 			document.body.classList.remove('nav-open');
 		})
 	})
+}
+closeNavBar();
 
-	async function typeSentence(sentence, eleRef, delay = 100) {
-		const letters = sentence.split("");
-		let i = 0;
-		while (i < letters.length) {
-			await waitForMs(delay);
-			$(eleRef).append(letters[i]);
-			i++
+
+// Methods to deal with typing animation
+
+const titleList = ["QA Engineer", "Software Developer", "Automation Developer"]
+
+async function typeSentence(sentence, eleRef, delay = 100) {
+	const letters = sentence.split("");
+	let i = 0;
+	while (i < letters.length) {
+		await waitForMs(delay);
+		$(eleRef).append(letters[i]);
+		i++
+	}
+	return;
+}
+
+async function deleteSentence(eleRef) {
+	const sentence = $(eleRef).html();
+	const letters = sentence.split("");
+	let i = 0;
+	while (letters.length > 0) {
+		await waitForMs(100)
+		letters.pop();
+		$(eleRef).html(letters.join(""));
+	}
+}
+
+function waitForMs(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function infiniteGenerateTitles(titleList, eleRef) {
+	var i = 0;
+	while(true) {
+		await typeSentence(titleList[i], eleRef);
+		await waitForMs(1500);
+		await deleteSentence(eleRef)
+		await waitForMs(500);
+		i++
+		if (i >= titleList.length) {
+			i = 0;
 		}
-		return;
 	}
+}
 
-	async function deleteSentence(eleRef) {
-		const sentence = $(eleRef).html();
-		const letters = sentence.split("");
-		let i = 0;
-		while (letters.length > 0) {
-			await waitForMs(100)
-			letters.pop();
-			$(eleRef).html(letters.join(""));
-		}
-	}
-
-	function waitForMs(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms))
-	}
-
-	const titleList = ["QA Engineer", "Software Developer", "Food Enthusiast"]
-
-	async function infiniteGenerateTitles(titleList, eleRef) {
-		var i = 0;
-		while(true) {
-			await typeSentence(titleList[i], eleRef);
-			await waitForMs(1500);
-			await deleteSentence(eleRef)
-			await waitForMs(500);
-			i++
-			if (i >= titleList.length) {
-				i = 0;
-			}
-		}
-	}
-
-	infiniteGenerateTitles(titleList, "#sentence");
-// })
+infiniteGenerateTitles(titleList, "#sentence");
